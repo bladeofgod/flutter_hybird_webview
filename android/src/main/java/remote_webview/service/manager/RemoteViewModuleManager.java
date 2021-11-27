abbf63a8eef840c0ec8a54672bfdd54ba9158747
bladeofgod <450103187@qq.com>
2021-11-27 22:15:47 +0800

    dev: add patrol request

--- android/src/main/java/remote_webview/service/manager/RemoteViewModuleManager.java
+++ android/src/main/java/remote_webview/service/manager/RemoteViewModuleManager.java
@@ -1,6 +1,12 @@
 package remote_webview.service.manager;
 
+import android.os.Handler;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+
 import java.util.HashMap;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -28,7 +34,23 @@ public class RemoteViewModuleManager {
 
     private ViewSavedInstance savedInstance = new ViewSavedInstance();
 
-    private RemoteViewModuleManager() {}
+    Handler handler;
+
+    private RemoteViewModuleManager() {
+        handler = new Handler(callback);
+    }
+
+
+    private Handler.Callback callback = new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message message) {
+            switch (message.what) {
+                //todo need patrol type (diff patrol time)
+            }
+            return true;
+        }
+    };
+
 
     public HashMap getSavedInstance() {
         return savedInstance.getSavedInstance();
@@ -36,6 +58,20 @@ public class RemoteViewModuleManager {
 
     public void setSavedInstance(HashMap status) {
         savedInstance.setSavedInstance(status);
+    }
+
+
+    private static class PatrolRequest {
+
+        RemoteViewModuleManager manager;
+        //unit: milliseconds
+        int patrolTime;
+
+
+        PatrolRequest(RemoteViewModuleManager moduleManager, int patrolTime) {
+            this.manager = moduleManager;
+            this.patrolTime = patrolTime;
+        }
     }
 
 
@@ -75,7 +111,21 @@ public class RemoteViewModuleManager {
             return instance;
         }
 
-        private void patrol() {}
+        private ArrayBlockingQueue<PatrolRequest> mQueue = new ArrayBlockingQueue<>(10);
+
+        private void patrol() {
+            PatrolRequest request;
+            try {
+                request = mQueue.take();
+                //do patrol
+                sleep(request.patrolTime);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+            Message.obtain(request.manager.handler, 0);
+
+        }
 
         @Override
         public void run() {
