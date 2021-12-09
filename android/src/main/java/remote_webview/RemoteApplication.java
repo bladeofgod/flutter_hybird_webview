8244860d007f705018aaa857e6b57bf9af2dd011
李佳奇 <450103187@qq.com>
2021-12-09 14:34:02 +0800

    def: add comment

--- android/src/main/java/remote_webview/RemoteApplication.java
+++ android/src/main/java/remote_webview/RemoteApplication.java
@@ -7,10 +7,16 @@ import android.widget.Toast;
 import remote_webview.input_hook.InputMethodHolder;
 import remote_webview.input_hook.OnInputMethodListener;
 import remote_webview.utils.LogUtil;
+import remote_webview.view.controller.InputToggleDelegate;
 
 public class RemoteApplication extends Application {
     OnInputMethodListener onInputMethodListener;
 
+    /**
+     * About the code in this {@link #getSystemService(String)}.
+     * @see InputToggleDelegate#inputServiceCall() 
+     *
+     */
     @Override
     protected void attachBaseContext(Context base) {
         int pid = android.os.Process.myPid();
